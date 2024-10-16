0cc1f68a9c50e5d60c4fb39885efbf0e44d273d2
Michael <michaeldiputra257@gmail.com>
2024-10-16 14:18:45 +0800

    add route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\LandingPage\LandingPage;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', LandingPage::class);
