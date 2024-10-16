abf4be35bd0778f5413d658c39ebcb70ad3f0613
Michael <michaeldiputra257@gmail.com>
2024-10-16 15:03:02 +0800

    add custome color, add font family, add component

--- resources/views/livewire/landing-page/components/home.blade.php
+++ resources/views/livewire/landing-page/components/home.blade.php
@@ -1,5 +1,12 @@
-<div>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+<div class="bg-cmyBlue-100 text-cmyBlue-900 flex justify-center px-5 md:px-12 lg:px-20 xl:px-32 font-cmyPoppins">
+    <div class="max-w-7xl w-full min-h-screen flex items-center">
+        <div class="w-4/5">
+            <h1 class="font-cmyRaleway text-7xl font-extrabold mb-5">Lorem <span class="text-cmyBlue-500">Ipsum</span> dolor sit amet</h1>
+            <p class="text-xl mb-11 text-cmySlate-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac convallis augue.</p>
+            <div class="grid grid-cols-2 w-fit gap-8">
+                <button class="bg-cmyBlue-500 rounded-full text-cmyBlue-100 py-5 px-7 font-bold">Start Growing Now</button>
+                <button class="border-cmyBlue-500 border-2 rounded-full text-cmyBlue-500 py-5 px-7 font-bold">Free Trial</button>
+            </div>
+        </div>
+    </div>
 </div>
