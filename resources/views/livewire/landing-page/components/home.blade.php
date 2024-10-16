113a5bb4adcc607f58d3b531e25dc9ecfc86f61f
Michael <michaeldiputra257@gmail.com>
2024-10-16 17:54:13 +0800

    add hover and make it responsive

--- resources/views/livewire/landing-page/components/home.blade.php
+++ resources/views/livewire/landing-page/components/home.blade.php
@@ -1,11 +1,11 @@
 <div class="bg-cmyBlue-100 text-cmyBlue-900 flex justify-center px-5 md:px-12 lg:px-20 xl:px-32 font-cmyPoppins">
     <div class="max-w-7xl w-full min-h-screen flex items-center">
-        <div class="w-4/5">
-            <h1 class="font-cmyRaleway text-7xl font-extrabold mb-5">Lorem <span class="text-cmyBlue-500">Ipsum</span> dolor sit amet</h1>
-            <p class="text-xl mb-11 text-cmySlate-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac convallis augue.</p>
-            <div class="grid grid-cols-2 w-fit gap-8">
-                <button class="bg-cmyBlue-500 rounded-full text-cmyBlue-100 py-5 px-7 font-bold">Start Growing Now</button>
-                <button class="border-cmyBlue-500 border-2 rounded-full text-cmyBlue-500 py-5 px-7 font-bold">Free Trial</button>
+        <div class="max-w-4xl">
+            <h1 class="title mb-3 md:mb-4 lg:mb-5">Lorem <span class="text-cmyBlue-500">Ipsum</span> dolor sit amet</h1>
+            <p class="description mb-8 sm:mb-9 md:mb-10 lg:mb-11">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac convallis augue.</p>
+            <div class="grid sm:w-fit gap-3 sm:gap-4 md:gap-6 lg:gap-8 grid-cols-1 sm:grid-cols-2">
+                <button class="text-cmyBase bg-cmyBlue-500 rounded-full text-cmyBlue-100 py-3 sm:py-4 md:py-4 lg:py-5 px-4 sm:px-5 md:px-6 lg:px-7 font-bold transition duration-300 hover:scale-105">Start Growing Now</button>
+                <button class="text-cmyBase border-cmyBlue-500 border-2 rounded-full text-cmyBlue-500 py-3 sm:py-4 md:py-4 lg:py-5 px-4 sm:px-5 md:px-6 lg:px-7 font-bold bg-transparent transition duration-300 hover:text-cmyBlue-100 hover:bg-cmyBlue-500">Free Trial</button>
             </div>
         </div>
     </div>
