44bf2adba068774d6c4ffafd4f8af537cfc9eaa9
Michael <michaeldiputra257@gmail.com>
2024-11-11 12:40:17 +0800

    make it responsive

--- resources/views/livewire/landing-page/components/features.blade.php
+++ resources/views/livewire/landing-page/components/features.blade.php
@@ -2,13 +2,13 @@
     <div class="max-w-7xl w-full">
         <div class="w-full max-w-2xl">
             <h2 class="">Organic interaction with your target audience</h2>
-            <p class="text-cmyLg">We take care of your growth 24/7, so you can focus on what really matters.</p>
+            <p class="text-cmyLg mt-3 md:mt-6">We take care of your growth 24/7, so you can focus on what really matters.</p>
         </div>
-        <div class="mt-20 gap-10 grid grid-cols-3">
-            <div class="skeleton w-full h-96 col-span-2"></div>
+        <div class="mt-10 md:mt-20 grid gap-8 grid-cols-1 md:gap-10 md:grid-cols-3">
+            <div class="skeleton w-full h-96 col-span-1 md:col-span-2"></div>
             <div class="skeleton w-full h-96 "></div>
             <div class="skeleton w-full h-96 "></div>
-            <div class="skeleton w-full h-96 col-span-2"></div>
+            <div class="skeleton w-full h-96 col-span-1 md:col-span-2"></div>
         </div>
     </div>
 </div>
