2eeb6b9e25bc52ea618413e08e47fb2acea2e372
Michael <michaeldiputra257@gmail.com>
2024-11-18 17:53:55 +0800

    create faq component, add boostrap-icons

---
+++ app/Livewire/LandingPage/Components/Faq.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\LandingPage\Components;
+
+use Livewire\Component;
+
+class Faq extends Component
+{
+    public function render()
+    {
+        return view('livewire.landing-page.components.faq');
+    }
+}
---
+++ resources/views/livewire/landing-page/components/faq.blade.php
@@ -0,0 +1,26 @@
+<div class="bg-cmyBlue-100 text-cmyBlue-900 flex justify-center py-24 px-5 md:px-12 lg:px-20 xl:px-32 font-cmyPoppins">
+    <div class="max-w-7xl w-full grid grid-cols-2">
+        <div class="">
+            <h2>Frequently asked questions</h2>
+            <p>Can't find the answer you're looking for? Feel free to book a call!</p>
+        </div>
+        <div class="flex flex-col justify-end">
+            <div class="flex justify-between items-center border-cmyBlue-900 border-b-2 pb-3 pt-8">
+                <p class="text-cmy2xl text text-cmyBlue-900 font-semibold size-">Lorem ipsum dolor sit amet.</p>
+                <x-bi-arrow-down-circle class="size-5 mr-2.5"/>
+            </div>
+            <div class="flex justify-between items-center border-cmyBlue-900 border-b-2 pb-3 pt-8">
+                <p class="text-cmy2xl text text-cmyBlue-900 font-semibold size-">Lorem ipsum dolor sit amet.</p>
+                <x-bi-arrow-down-circle class="size-5 mr-2.5"/>
+            </div>
+            <div class="flex justify-between items-center border-cmyBlue-900 border-b-2 pb-3 pt-8">
+                <p class="text-cmy2xl text text-cmyBlue-900 font-semibold size-">Lorem ipsum dolor sit amet.</p>
+                <x-bi-arrow-down-circle class="size-5 mr-2.5"/>
+            </div>
+            <div class="flex justify-between items-center border-cmyBlue-900 border-b-2 pb-3 pt-8">
+                <p class="text-cmy2xl text text-cmyBlue-900 font-semibold size-">Lorem ipsum dolor sit amet.</p>
+                <x-bi-arrow-down-circle class="size-5 mr-2.5"/>
+            </div>
+        </div>
+    </div>
+</div>
