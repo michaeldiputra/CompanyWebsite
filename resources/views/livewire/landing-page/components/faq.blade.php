0114e6fba84ff034a3df68ba5c1291b9a287867f
Michael <michaeldiputra257@gmail.com>
2024-11-19 09:41:04 +0800

    add faqs, add foreach

--- resources/views/livewire/landing-page/components/faq.blade.php
+++ resources/views/livewire/landing-page/components/faq.blade.php
@@ -5,22 +5,17 @@
             <p>Can't find the answer you're looking for? Feel free to book a call!</p>
         </div>
         <div class="flex flex-col justify-end">
-            <div class="flex justify-between items-center border-cmyBlue-900 border-b-2 pb-3 pt-8">
-                <p class="text-cmy2xl text text-cmyBlue-900 font-semibold size-">Lorem ipsum dolor sit amet.</p>
-                <x-bi-arrow-down-circle class="size-5 mr-2.5"/>
-            </div>
-            <div class="flex justify-between items-center border-cmyBlue-900 border-b-2 pb-3 pt-8">
-                <p class="text-cmy2xl text text-cmyBlue-900 font-semibold size-">Lorem ipsum dolor sit amet.</p>
-                <x-bi-arrow-down-circle class="size-5 mr-2.5"/>
-            </div>
-            <div class="flex justify-between items-center border-cmyBlue-900 border-b-2 pb-3 pt-8">
-                <p class="text-cmy2xl text text-cmyBlue-900 font-semibold size-">Lorem ipsum dolor sit amet.</p>
-                <x-bi-arrow-down-circle class="size-5 mr-2.5"/>
-            </div>
-            <div class="flex justify-between items-center border-cmyBlue-900 border-b-2 pb-3 pt-8">
-                <p class="text-cmy2xl text text-cmyBlue-900 font-semibold size-">Lorem ipsum dolor sit amet.</p>
-                <x-bi-arrow-down-circle class="size-5 mr-2.5"/>
-            </div>
+            @foreach ($faqs as $index => $faq)
+                <div class="border-cmyBlue-900 border-b-2 transition pb-2" x-data="{ open: false }">
+                    <button @click="open = !open" class="w-full flex justify-between items-center pb-1 pt-8">
+                        <p class="text-cmy2xl text text-cmyBlue-900 font-semibold">{{ $faq['question'] }}</p>
+                        <div class="" :class="open ? 'transform rotate-180 mr-2.5' : 'transform rotate-0 mr-2.5'">
+                            <x-bi-arrow-down-circle class="size-5" />
+                        </div>
+                    </button>
+                    <p class="" x-show="open">{{ $faq['answer'] }}</p>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
