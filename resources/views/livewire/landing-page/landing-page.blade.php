f7b7392fbf44da5a66181312c49aecb68a198007
Michael <michaeldiputra257@gmail.com>
2024-11-19 09:43:52 +0800

    remove comment

--- resources/views/livewire/landing-page/landing-page.blade.php
+++ resources/views/livewire/landing-page/landing-page.blade.php
@@ -1,5 +1,5 @@
 <div>
-    {{-- @livewire('landing-page.components.home') --}}
-    {{-- @livewire('landing-page.components.features') --}}
+    @livewire('landing-page.components.home')
+    @livewire('landing-page.components.features')
     @livewire('landing-page.components.faq')
 </div>
