98390336b00f28e932c52e200e431c9130ad0930
Michael <michaeldiputra257@gmail.com>
2024-12-20 09:41:50 +0800

    create footer component for dekstop view

---
+++ resources/views/livewire/landing-page/components/footer.blade.php
@@ -0,0 +1,29 @@
+<div class="h-screen -my-6">
+    <div
+        class="bg-cmyBlue-100 text-cmyBlue-900 flex justify-center py-16 px-5 md:px-12 lg:px-20 xl:px-32 font-cmyPoppins h-full ">
+        <div class="max-w-7xl w-full flex flex-col items-center">
+            <div class="flex flex-col items-center max-w-2xl h-full justify-center mb-6">
+                <p class="text-3xl font-medium text-cmyBlue-900 mb-3">Logo</p>
+                <p class="font-semibold text-4xl text-center text-cmyBlue-900 mb-14">start to gain new experiences in a
+                    new world</p>
+                <form class="w-full flex gap-2 px-12" action="">
+                    <input
+                        class="w-full rounded-full bg-cmyBlue-900/15 text-cmyLg text-cmyBlue-900 p-3 px-5 focus:outline-none placeholder:text-cmyBlue-100"
+                        type="email" name="email" id="email" placeholder="Enter your email">
+                    <button
+                        class="rounded-full w-fit text-nowrap bg-cmyBlue-900 text-cmyLg text-white p-3 px-5 focus:outline-none"
+                        type="submit">Try Now</button>
+                </form>
+            </div>
+            <div class="flex gap-20 !text-cmyBlue-900 py-2">
+                <a href="#">Features</a>
+                <a href="#">About Us</a>
+                <a href="#">Procedure</a>
+                <a href="#">Benefit</a>
+                <a href="#">Gallery</a>
+                <a href="#">FAQ</a>
+            </div>
+        </div>
+    </div>
+    <div class="bg-cmyBlue-900 h-6"></div>
+</div>
