4bc3a0f3ea1b926f0f5beb6991ba22bdc2adaa38
Michael <michaeldiputra257@gmail.com>
2024-12-29 15:06:45 +0800

    create counter component, edit app.css. add cmy3xl and cmyBase

---
+++ app/Livewire/LandingPage/Components/Counter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\LandingPage\Components;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public function render()
+    {
+        return view('livewire.landing-page.components.counter');
+    }
+}
---
+++ resources/views/livewire/landing-page/components/counter.blade.php
@@ -0,0 +1,30 @@
+<div class="bg-cmyBlue-900 text-white flex justify-center py-24 px-5 md:px-12 lg:px-20 xl:px-32 font-cmyPoppins">
+    <div class="max-w-7xl w-full flex justify-between items-center font-semibold gap-x-4">
+        <div class="text-wrap text-cmy2xl w-full">
+            Our great achievement to show
+        </div>
+        <div class="flex w-fit justify-end gap-x-6">
+            <div class="bg-white rounded-full w-0.5 my-2 sm:hidden"></div>
+            <div class="flex flex-col justify-center items-center">
+                <div class="text-cmy4xl">
+                    2150+
+                </div>
+                <div class="text-cmyBase text-nowrap">Student get jobs</div>
+            </div>
+            <div class="bg-white rounded-full w-0.5 my-2"></div>
+            <div class="flex flex-col justify-center items-center">
+                <div class="text-cmy4xl">
+                    2150+
+                </div>
+                <div class="text-cmyBase text-nowrap">Student get jobs</div>
+            </div>
+            <div class="bg-white rounded-full w-0.5 my-2"></div>
+            <div class="flex flex-col justify-center items-center">
+                <div class="text-cmy4xl">
+                    2150+
+                </div>
+                <div class="text-cmyBase text-nowrap">Student get jobs</div>
+            </div>
+        </div>
+    </div>
+</div>
