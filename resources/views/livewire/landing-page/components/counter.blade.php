b29070914cd8d022b8b06f9f9438d7e915e331d5
Michael <michaeldiputra257@gmail.com>
2025-01-09 21:33:12 +0800

    make it responsive, customize landing page order

--- resources/views/livewire/landing-page/components/counter.blade.php
+++ resources/views/livewire/landing-page/components/counter.blade.php
@@ -1,29 +1,36 @@
-<div class="bg-cmyBlue-900 text-white flex justify-center py-24 px-5 md:px-12 lg:px-20 xl:px-32 font-cmyPoppins">
-    <div class="max-w-7xl w-full flex justify-between items-center font-semibold gap-x-4">
-        <div class="text-wrap text-cmy2xl w-full">
+<div class="bg-cmyBlue-900 text-white flex justify-center py-8 md:py-24 px-5 md:px-12 lg:px-20 xl:px-32 font-cmyPoppins">
+    <div
+        class="max-w-7xl w-full flex justify-between items-start sm:items-center font-semibold gap-x-4 flex-col pb-3 md:flex-row md:pb-0">
+        <div class="text-wrap text-cmy2xl w-full pb-4 text-left sm:text-center md:text-left md:pb-0">
             Our great achievement to show
         </div>
-        <div class="flex w-fit justify-end gap-x-6">
-            <div class="bg-white rounded-full w-0.5 my-2 sm:hidden"></div>
-            <div class="flex flex-col justify-center items-center">
-                <div class="text-cmy4xl">
-                    2150+
+        <div class="flex w-fit justify-end gap-y-2 gap-x-6 flex-col sm:flex-row">
+            <div class="flex">
+                <div class="bg-white rounded-full w-0.5 mr-3 sm:mr-0 sm:my-2 sm:hidden"></div>
+                <div class="flex flex-col justify-center sm:items-center">
+                    <div class="text-cmy4xl">
+                        2150+
+                    </div>
+                    <div class="text-cmyBase text-nowrap">Student get jobs</div>
                 </div>
-                <div class="text-cmyBase text-nowrap">Student get jobs</div>
             </div>
-            <div class="bg-white rounded-full w-0.5 my-2"></div>
-            <div class="flex flex-col justify-center items-center">
-                <div class="text-cmy4xl">
-                    2150+
+            <div class="flex">
+                <div class="bg-white rounded-full w-0.5 mr-3 sm:mr-0 sm:my-2 sm:hidden"></div>
+                <div class="flex flex-col justify-center sm:items-center">
+                    <div class="text-cmy4xl">
+                        2150+
+                    </div>
+                    <div class="text-cmyBase text-nowrap">Student get jobs</div>
                 </div>
-                <div class="text-cmyBase text-nowrap">Student get jobs</div>
             </div>
-            <div class="bg-white rounded-full w-0.5 my-2"></div>
-            <div class="flex flex-col justify-center items-center">
-                <div class="text-cmy4xl">
-                    2150+
+            <div class="flex">
+                <div class="bg-white rounded-full w-0.5 mr-3 sm:mr-0 sm:my-2 sm:hidden"></div>
+                <div class="flex flex-col justify-center sm:items-center">
+                    <div class="text-cmy4xl">
+                        2150+
+                    </div>
+                    <div class="text-cmyBase text-nowrap">Student get jobs</div>
                 </div>
-                <div class="text-cmyBase text-nowrap">Student get jobs</div>
             </div>
         </div>
     </div>
