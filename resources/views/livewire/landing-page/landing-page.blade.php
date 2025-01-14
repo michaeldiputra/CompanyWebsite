4829ac358ee835c3b6e09137f8b6a2ef93c77fa9
Michael <michaeldiputra257@gmail.com>
2025-01-14 21:39:30 +0800

    First Commit

--- resources/views/livewire/landing-page/landing-page.blade.php
+++ resources/views/livewire/landing-page/landing-page.blade.php
@@ -2,7 +2,7 @@
     @livewire('landing-page.components.home')
     @livewire('landing-page.components.about')
     @livewire('landing-page.components.counter')
-    @livewire('landing-page.components.features')
+    {{-- @livewire('landing-page.components.features') --}}
     @livewire('landing-page.components.faq')
     @livewire('landing-page.components.footer')
 </div>
